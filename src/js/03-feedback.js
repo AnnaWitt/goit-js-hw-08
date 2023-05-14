e4169cf43120fccef3f119d1188b4abb97d823f0
Anna Witt <110746108+AnnaWitt@users.noreply.github.com>
2023-05-15 01:43:06 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const saveFormDataToLocalStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+form.addEventListener('input', saveFormDataToLocalStorage);
+
+window.addEventListener('load', () => {
+  const storageFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (storageFormData) {
+    const parsedFormData = JSON.parse(storageFormData);
+    emailInput.value = parsedFormData.email;
+    messageInput.value = parsedFormData.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+
+  emailInput.value = '';
+  messageInput.value = '';
+});
